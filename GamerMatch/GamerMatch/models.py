30deb582dcda4f5cdb13cb26282a06a96ac46012
Nacho <inunezn@fen.uchile.cl>
2021-05-02 17:14:59 -0400

    User text size corregido

--- GamerMatch/GamerMatch/models.py
+++ GamerMatch/GamerMatch/models.py
@@ -6,7 +6,7 @@ class MatchForm(models.Model):
     juego = models.CharField(max_length=100)
     tags = models.TextField(max_length=100)
     descripcion = models.TextField(max_length=200)
-    user = models.CharField(max_length=100, default='')
+    user = models.CharField(max_length=150, default='')
     time = models.DateTimeField(auto_now=True)
 
 
